f9e936ac83d6fed020e3f6eb66f0ea6789c1ac1f
Narendra Sharma <ns1512316@gmail.com>
2024-11-28 19:52:22 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Contacts from './components/Contacts'
 import toast, { Toaster } from 'react-hot-toast'
 
